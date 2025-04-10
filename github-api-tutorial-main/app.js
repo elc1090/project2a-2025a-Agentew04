163c2eac02b33364e338c6a5048ee66873f4e3ab
Rodrigo Appelt <31523279+Agentew04@users.noreply.github.com>
2025-04-10 19:21:34 -0300

    add commit visualization

--- github-api-tutorial-main/app.js
+++ github-api-tutorial-main/app.js
@@ -12,10 +12,13 @@ loadCommitsButton.disabled = true;
 
 let state = {
     repos: [],
+    commits: [],
     username: '',
     repoMessage: null,
     isRepoError: false,
     selectedRepo: null,
+    commitMessage: null,
+    isCommitError: false,
 }
 
 usernameInput.addEventListener('keyup', (e) => {
@@ -23,27 +26,31 @@ usernameInput.addEventListener('keyup', (e) => {
     UpdateState(state);
 });
 
-repoSelect.addEventListener('change', (e) => {
-    const selectedRepo = e.target.value;
-    console.log("Selected repo:", selectedRepo);
-    if (selectedRepo !== 'default') {
-        state.selectedRepo = selectedRepo;
-        loadCommitsButton.disabled = false;
-        loadCommitsButton.addEventListener('click', () => {
-            const selectedRepoName = e.target.value;
-            console.log("Loading commits for repo:", selectedRepoName);   
-            ShowCommits();
-        }
-        );
-    } else {
-        loadCommitsButton.disabled = true;
-    }
-});
+repoSelect.addEventListener('change', OnRepoSelected);
             
 
 // Listen for clicks on the fetch repos button
 fetchReposButton.addEventListener('click', FetchRepos);
 showReposButton.addEventListener('click', ShowRepos);
+loadCommitsButton.addEventListener('click', LoadCommits);
+
+function OnRepoSelected(e){
+    const selectedRepo = e.target.value;
+    console.log("Selected repo:", selectedRepo);
+    if (selectedRepo !== 'default') {
+        state.selectedRepo = selectedRepo;
+        loadCommitsButton.disabled = false;
+        loadCommitsButton.addEventListener('click', LoadCommits);
+    } else {
+        loadCommitsButton.disabled = true;
+    }
+}
+
+function LoadCommits(e){
+    const selectedRepoName = e.target.value;
+    console.log("Loading commits for repo:", selectedRepoName);   
+    ShowCommits();
+}
 
 function UpdateState(){
     showReposButton.disabled = state.repos.length === 0 || !state.username;
@@ -89,7 +96,75 @@ function UpdateState(){
 }
 
 async function ShowCommits(){
+    state.username = usernameInput.value;
+    state.commits = [];
+    const ul = document.getElementById('userRepos');
+    ul.innerHTML = '';
+    console.log("Fetching commits for repo:", state.selectedRepo);
+    try{
+        let response = await fetch(`https://api.github.com/repos/${state.username}/${state.selectedRepo}/commits`);
 
+        if(response.status == 403) {
+            // forbidden, rate limit exceeded
+            console.error('Rate limit exceeded. Please try again later.');
+            state.isCommitError = true;
+            state.commitMessage = 'Rate limit excedido. Tente novamente mais tarde.';
+            UpdateState();
+            return;
+        }
+
+        if(response.status == 404){
+            console.error('Repo not found:', state.selectedRepo);
+            state.isCommitError = true;
+            state.commitMessage = 'Repositório não encontrado: ' + state.selectedRepo;
+            UpdateState();
+            return;
+        }
+
+        if(response.status != 200){
+            console.error('Error fetching data:', response.statusText);
+            state.isCommitError = true;
+            state.commitMessage = 'Erro ' + response.statusText;
+            UpdateState();
+            return;
+        }
+
+        let data = await response.json();
+        
+        if(data.message === "Not Found"){
+            state.isCommitError = true;
+            state.commitMessage = 'Repositório não encontrado: ' + state.username;
+            console.error('Repo not found, code 200:', state.username);
+            UpdateState();
+            return;
+        }
+
+        for (const commit of data) {
+            state.commits.push(commit);
+        }
+
+        console.log("Commits fetched successfully:", state.commits);
+        state.commitMessage = 'Buscado '+ state.commits.length + ' commits';
+        state.isCommitError = false;
+        UpdateState();
+
+        for (const commit of state.commits) {
+            const li = document.createElement('li');
+            li.classList.add('list-group-item');
+            li.innerHTML = `
+                <p><strong>Hash:</strong> ${commit.sha}</p>
+                <p><strong>Autor:</strong> ${commit.commit.author.name}</p>
+                <p><strong>Data:</strong> ${commit.commit.author.date}</p>
+                <p><strong>Mensagem:</strong> ${commit.commit.message}</p>
+            `;
+            ul.appendChild(li);
+        }
+        
+        
+
+    }catch(error){
+
+    }
 }
 
 async function FetchRepos(){
@@ -105,21 +180,21 @@ async function FetchRepos(){
             // forbidden, rate limit exceeded
             console.error('Rate limit exceeded. Please try again later.');
             state.isRepoError = true;
-            state.repoMessage = 'Rate limit exceeded. Please try again later.';
+            state.repoMessage = 'Rate limit excedido. Tente novamente mais tarde.';
             UpdateState();
             return;
         }
         if(response.status == 404){
             console.error('User not found:', state.username);
             state.isRepoError = true;
-            state.repoMessage = 'User not found: ' + state.username;
+            state.repoMessage = 'Usuário não encontrado: ' + state.username;
             UpdateState();
             return;
         }
         if(response.status != 200){
             console.error('Error fetching data:', response.statusText);
             state.isRepoError = true;
-            state.repoMessage = 'Error ' + response.statusText;
+            state.repoMessage = 'Erro ' + response.statusText;
             UpdateState();
             return;
         }
@@ -128,8 +203,8 @@ async function FetchRepos(){
 
         if(data.message === "Not Found"){
             state.isRepoError = true;
-            state.repoMessage = 'No account exists with username: ' + state.username;
-            console.error('No account exists with username:', state.username);
+            state.repoMessage = 'Usuário não encontrado: ' + state.username;
+            console.error('No account exists with username, code 200:', state.username);
             UpdateState();
             return;
         }
@@ -138,7 +213,7 @@ async function FetchRepos(){
         }
         console.log("Repos fetched successfully:", state.repos);
         state.isRepoError = false;
-        state.repoMessage = 'Fetched '+ state.repos.length + ' repos';
+        state.repoMessage = 'Buscado '+ state.repos.length + ' repositórios';
         UpdateState();
     }catch(error){  
         console.error('Error fetching data:', error);
@@ -157,57 +232,10 @@ function ShowRepos(){
         const li = document.createElement('li');
         li.classList.add('list-group-item');
         li.innerHTML = `
-            <p><strong>Repo:</strong> ${repo.name}</p>
-            <p><strong>Description:</strong> ${repo.description || 'No description available'}</p>
+            <p><strong>Nome:</strong> ${repo.name}</p>
+            <p><strong>Descrição:</strong> ${repo.description || 'No description available'}</p>
             <p><strong>URL:</strong> <a href="${repo.html_url}">${repo.html_url}</a></p>
         `;
         ul.appendChild(li);
     }
 }
-
-// // Get the GitHub username input form
-
-// // Listen for submissions on GitHub username input form
-// gitHubForm.addEventListener('submit', (e: Event) => {
-//     // Prevent default form submission action
-//     e.preventDefault();
-
-//     // Get the GitHub username input field on the DOM
-//     const usernameInput = document.getElementById('usernameInput') as HTMLInputElement;
-
-//     // Get the value of the GitHub username input field
-//     const gitHubUsername = usernameInput.value;
-
-//     // Run GitHub API function, passing in the GitHub username
-//     requestUserRepos(gitHubUsername)
-//         .then(response => response.json()) // parse response into json
-//         .then(data => {
-//             const ul = document.getElementById('userRepos') as HTMLUListElement;
-//             ul.innerHTML = ''; // Clear previous results
-
-//             if (data.message === "Not Found") {
-//                 const li = document.createElement('li');
-//                 li.classList.add('list-group-item');
-//                 li.innerHTML = `<p><strong>No account exists with username:</strong> ${gitHubUsername}</p>`;
-//                 ul.appendChild(li);
-//             } else {
-//                 for (const repo of data) {
-//                     const li = document.createElement('li');
-//                     li.classList.add('list-group-item');
-//                     li.innerHTML = `
-//                         <p><strong>Repo:</strong> ${repo.name}</p>
-//                         <p><strong>Description:</strong> ${repo.description || 'No description available'}</p>
-//                         <p><strong>URL:</strong> <a href="${repo.html_url}">${repo.html_url}</a></p>
-//                     `;
-//                     ul.appendChild(li);
-//                 }
-//             }
-//         })
-//         .catch(error => {
-//             console.error('Error fetching data:', error);
-//         });
-// });
-
-// function requestUserRepos(username: string): Promise<Response> {
-//     return fetch(`https://api.github.com/users/${username}/repos`);
-// }
